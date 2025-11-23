1b46f9d086ec0a401844bdacfc9da6fbbd499551
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2025-11-23 17:45:42 +0100

    feat(types): add AMQP Config type and update type aliases

--- types.go
+++ types.go
@@ -2,7 +2,11 @@ package broker
 
 import amqp "github.com/rabbitmq/amqp091-go"
 
-type Args = amqp.Table
-type Connection = amqp.Connection
-type Channel = amqp.Channel
-type Delivery = amqp.Delivery
+/* External types */
+type (
+	Arguments  = amqp.Table
+	Config     = amqp.Config
+	Connection = amqp.Connection
+	Channel    = amqp.Channel
+	Error      = amqp.Error
+)
