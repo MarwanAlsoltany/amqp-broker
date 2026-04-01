bdf814c4091ce96d25e1b674f66cd5ffbd10b7de
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2026-04-01 20:11:27 +0200

    feat(topology): add shared type aliases

---
+++ internal/topology/types.go
@@ -0,0 +1,10 @@
+package topology
+
+import (
+	"github.com/MarwanAlsoltany/amqp-broker/internal/transport"
+)
+
+type (
+	// Arguments is a type alias for AMQP arguments used in topology declarations.
+	Arguments = transport.Arguments
+)
