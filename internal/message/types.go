12682eb54168094ae1525a5900fca28f02b8b5d3
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2026-04-03 19:55:26 +0200

    feat(message): add shared type aliases

---
+++ internal/message/types.go
@@ -0,0 +1,10 @@
+package message
+
+import (
+	"github.com/MarwanAlsoltany/amqp-broker/internal/transport"
+)
+
+type (
+	// Arguments is a type alias for AMQP arguments used in message headers.
+	Arguments = transport.Arguments
+)
